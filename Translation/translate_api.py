ce3b55debc8f42a2352b2d19c9b1b5454751d826
felixzhao <felixzhao@google.com>
2020-04-24 11:25:53 +0800

    translate v2

---
+++ Translation/translate_api.py
@@ -0,0 +1,23 @@
+from google.cloud import translate_v2 as translate
+import os,sys
+import time
+
+
+
+# Instantiates a client
+translate_client = translate.Client()
+
+# The target language
+source = 'en'
+target = 'zh'
+
+fp = open(u"hello_world.txt", 'r')
+# Translates some text into Russian
+for line in fp.readlines():
+  pretime = time.time()
+  text = line.strip()
+  translation = translate_client.translate(
+    text,
+    source_language=source,
+    target_language=target)
+  print(u'{}\t{}\t{}'.format(text, translation['translatedText'], time.time() - pretime))
